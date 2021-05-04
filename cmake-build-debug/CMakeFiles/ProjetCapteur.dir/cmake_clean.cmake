0347cac1e8d9d34b97f8f622287946f25d895acc
el Rifai Rami <rami.el-rifai@insa-lyon.fr>
2021-05-04 14:37:35 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProjetCapteur.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetCapteur.dir/main.cpp.obj"
+  "ProjetCapteur.exe"
+  "ProjetCapteur.exe.manifest"
+  "ProjetCapteur.pdb"
+  "libProjetCapteur.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetCapteur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
